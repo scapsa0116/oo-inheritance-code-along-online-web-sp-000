837306038d19efedc710f56b2cd647da2fdb3395
Stela Capsa <stela.capsa@gmail.com>
2020-01-12 14:17:53 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,8 +1,8 @@
 require_relative "./vehicle.rb"
 class Car 
-  
+  attr_accessor :wheel_size, :wheel_number
   def intialize(wheel_size, wheel_number)
-  @wheel_size =wheel_size
+  @wheel_size = wheel_size
   @wheel_number = wheel_number
 end 
 end 
